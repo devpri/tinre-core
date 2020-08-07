9e346ede0f1e609e41fce33d2b8026cb5a59577a
Paul Prisacaru <paul@devpri.com>
2020-08-07 23:22:25 +0300

    fix popover on ios

--- resources/views/partials/user.blade.php
+++ resources/views/partials/user.blade.php
@@ -1,13 +1,13 @@
 <div class="content-inner flex justify-end">
     @if(Auth::user() && Auth::user()->can('viewAny', \Devpri\Tinre\Models\User::class))
-      <v-popover placement="bottom-start">
+      <v-popover ref="settingsMenu" placement="bottom-start">
         <button class="pointer px-2 py-1 flex">
           <svg width="30" height="30" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1152 896q0-106-75-181t-181-75-181 75-75 181 75 181 181 75 181-75 75-181zm512-109v222q0 12-8 23t-20 13l-185 28q-19 54-39 91 35 50 107 138 10 12 10 25t-9 23q-27 37-99 108t-94 71q-12 0-26-9l-138-108q-44 23-91 38-16 136-29 186-7 28-36 28h-222q-14 0-24.5-8.5t-11.5-21.5l-28-184q-49-16-90-37l-141 107q-10 9-25 9-14 0-25-11-126-114-165-168-7-10-7-23 0-12 8-23 15-21 51-66.5t54-70.5q-27-50-41-99l-183-27q-13-2-21-12.5t-8-23.5v-222q0-12 8-23t19-13l186-28q14-46 39-92-40-57-107-138-10-12-10-24 0-10 9-23 26-36 98.5-107.5t94.5-71.5q13 0 26 10l138 107q44-23 91-38 16-136 29-186 7-28 36-28h222q14 0 24.5 8.5t11.5 21.5l28 184q49 16 90 37l142-107q9-9 24-9 13 0 25 10 129 119 165 170 7 8 7 22 0 12-8 23-15 21-51 66.5t-54 70.5q26 50 41 98l183 28q13 2 21 12.5t8 23.5z" fill="#fff"/></svg>
         </button>
         <template class="hidden" slot="popover">
           <ul class="text-gray-700 pt-1 mx-4">
-              <li class="">
-                  <router-link v-close-popover :to="{ name: 'users.index' }" class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap w-40">
+              <li>
+                  <router-link :to="{ name: 'users.index' }" @click.native="$refs.settingsMenu.hide()" class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap w-40">
                       {{ __('Users') }}
                   </router-link>
               </li>
@@ -15,18 +15,18 @@
         </template>
       </v-popover>
     @endif
-    <v-popover placement="bottom-start">
+    <v-popover ref="userMenu" placement="bottom-start">
       <button class="pointer px-2 py-1 flex">
         <svg width="30" height="30" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1536 1399q0 109-62.5 187t-150.5 78h-854q-88 0-150.5-78t-62.5-187q0-85 8.5-160.5t31.5-152 58.5-131 94-89 134.5-34.5q131 128 313 128t313-128q76 0 134.5 34.5t94 89 58.5 131 31.5 152 8.5 160.5zm-256-887q0 159-112.5 271.5t-271.5 112.5-271.5-112.5-112.5-271.5 112.5-271.5 271.5-112.5 271.5 112.5 112.5 271.5z" fill="#fff"/></svg>
       </button>
       <template class="hidden" slot="popover">
         <ul class="text-gray-700 pt-1 mx-4">
-            <li class="">
-                <router-link v-close-popover :to="{ name: 'profile' }" class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap w-40">
-                    {{ __('Profile') }}
+            <li>
+                <router-link :to="{ name: 'profile' }" @click.native="$refs.userMenu.hide()" class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap w-40">
+                  {{ __('Profile') }}
                 </router-link>
             </li>
-            <li class=""><a class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap w-40" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></li>
+            <li><a class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap w-40" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></li>
         </ul>
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
             @csrf
