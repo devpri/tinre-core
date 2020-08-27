62870135fe07cc787d848b858580641c3493b4db
Paul Prisacaru <paul@devpri.com>
2020-08-27 11:50:25 +0300

    update url controller

--- src/Http/Controllers/Web/UrlController.php
+++ src/Http/Controllers/Web/UrlController.php
@@ -6,7 +6,6 @@ use Devpri\Tinre\Http\Controllers\Controller;
 use Devpri\Tinre\Http\Resources\Web\Url as UrlResource;
 use Devpri\Tinre\Models\Url;
 use Devpri\Tinre\Services\UrlService;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class UrlController extends Controller
@@ -20,33 +19,7 @@ class UrlController extends Controller
 
     public function index(Request $request)
     {
-        $user = $request->user();
-
-        $search = $request->search;
-        $date = $request->date;
-
-        $query = Url::query();
-
-        if ($user->cant('viewAny', Url::class)) {
-            $query->where('user_id', $user->id);
-        }
-
-        if ($search) {
-            $query->where(function (Builder $query) use ($search) {
-                $query->where('path', 'LIKE', "%{$search}%")
-                    ->orWhere('long_url', 'LIKE', "%{$search}%")
-                    ->orWhereHas('user', function ($query) use ($search) {
-                        $query->where('name', 'LIKE', "%{$search}%")
-                            ->orWhere('email', 'LIKE', "%{$search}%");
-                    });
-            });
-        }
-
-        if ($date) {
-            $query->whereBetween('created_at', $date);
-        }
-
-        $urls = $query->where('active', $request->active ? $request->active : 1)->orderBy('created_at', 'DESC')->with('user')->paginate(30);
+        $urls = $this->urlService->index($request);
 
         return UrlResource::collection($urls)->additional(['authorized_actions' => (new Url)->authorizedActions()]);
     }
@@ -66,45 +39,15 @@ class UrlController extends Controller
 
     public function create(Request $request): UrlResource
     {
-        $user = $request->user();
-
-        if ($user && $user->cant('create', Url::class)) {
-            abort(401);
-        }
-
-        $validationRules = ['long_url' => ['required', 'url', 'active_url']];
-
-        if ($user || config('tinre.guest_form_custom_path')) {
-            $validationRules['path'] = ['nullable', 'alpha_dash', 'min:'.config('tinre.min_path_length'), 'max:'.config('tinre.max_path_length')];
-        }
-
-        $validatedData = $request->validate($validationRules);
-
-        $url = $this->urlService->create($validatedData, $user);
+        $url = $this->urlService->create($request);
 
         return (new UrlResource($url))->additional(['message' => 'The URL has been created.']);
     }
 
     public function update(Request $request, $id)
     {
-        $validatedData = $request->validate([
-            'active' => ['required', 'boolean'],
-            'long_url' => ['required', 'url', 'active_url'],
-            'path' => ['required', "unique:urls,path,{$id},id", 'min:'.config('tinre.min_path_length'), 'max:'.config('tinre.max_path_length')],
-        ]);
 
-        $user = $request->user();
-
-        $url = Url::where('id', $id)->firstOrFail();
-
-        if ($user->cant('update', $url)) {
-            abort(401);
-        }
-
-        $url->active = $validatedData['active'];
-        $url->long_url = $validatedData['long_url'];
-        $url->path = $validatedData['path'];
-        $url->save();
+        $url = $this->urlService->update($request, $id);
 
         return (new UrlResource($url))->additional(['message' => 'The URL has been updated.']);
     }
--- src/Services/UrlService.php
+++ src/Services/UrlService.php
@@ -3,24 +3,80 @@
 namespace Devpri\Tinre\Services;
 
 use Devpri\Tinre\Models\Url;
-use Devpri\Tinre\Models\User;
 use Exception;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
 
 class UrlService
 {
-    public function create(array $data, User $user = null): Url
-    {
-        $this->validateUrl($data['long_url']);
 
+    public function index(Request $request)
+    {
+        $request->validate([
+            'search' => ['nullable', 'string'],
+            'date' => ['nullable', 'array'],
+            'limit' => ['nullable', 'number', 'min:1', 'max:100'],
+            'active' => ['nullable', 'boolean'],
+            'sort_by' => ['nullable', 'in:created_at,updated_at,clicks'],
+            'sort_direction' => ['nullable', 'in:asc,desc']
+        ]);
+        
+        $user = $request->user();
+        $search = $request->search;
+        $date = $request->date;
+        $limit = $request->limit ?? 30;
+        $active =$request->active;
+        $sortBy = $request->sort_by ?? 'created_at';
+        $sortDirection = $request->sort_direction ?? 'desc';
+                
+        $query = Url::query();
+
+        if ($user->cant('viewAny', Url::class)) {
+            $query->where('user_id', $user->id);
+        }
+
+        if ($search) {
+            $query->where(function (Builder $query) use ($search) {
+                $query->where('path', 'LIKE', "%{$search}%")
+                    ->orWhere('long_url', 'LIKE', "%{$search}%")
+                    ->orWhereHas('user', function ($query) use ($search) {
+                        $query->where('name', 'LIKE', "%{$search}%")
+                            ->orWhere('email', 'LIKE', "%{$search}%");
+                    });
+            });
+        }
+
+        if ($date) {
+            $query->whereBetween('created_at', $date);
+        }
+        
+        if(isset($active)) {
+            $query->where('active', $active);
+        }
+        
+        return $query->orderBy($sortBy, $sortDirection)->with('user')->paginate($limit);
+    }
+
+    public function create(Request $request): Url
+    {
+        $this->validateUrl($request->long_url);
+
+        $user = $request->user();
+
+        if ($user && $user->cant('create', Url::class)) {
+            abort(401);
+        }
+        
         $url = new Url();
-        $url->long_url = $data['long_url'];
+        $url->long_url = $request->long_url;
         $url->user_id = $user ? $user->id : null;
 
-        if (isset($data['path'])) {
-            $url->path = $data['path'];
+        if ($request->path && ($user || config('tinre.guest_form_custom_path'))) {
+            $url->path = $request->path;
 
             $this->validatePath($url->path);
 
@@ -48,8 +104,42 @@ class UrlService
         }
     }
 
-    protected function validatePath($path)
+    public function update(Request $request, $id)
     {
+        $request->validate([
+            'active' => ['required', 'boolean'],
+        ]);
+
+        $this->validateUrl($request->long_url);
+        
+        $url = Url::where('id', $id)->firstOrFail();
+
+        if($url->path != $request->path) {
+            $this->validatePath($request->path);
+        }
+        
+        $user = $request->user();
+
+        if ($user->cant('update', $url)) {
+            abort(401);
+        }
+
+        $url->active = $request->active;
+        $url->long_url = $request->long_url;
+        $url->path = $request->path;
+        $url->save();
+
+        return $url;
+    }
+
+    protected function validatePath($path): void
+    {
+        Validator::make([
+            'path' => $path
+        ], [
+            'path' => ['required', 'alpha_dash', 'min:'.config('tinre.min_path_length'), 'max:'.config('tinre.max_path_length')],
+        ])->validate();
+        
         $path = strtolower($path);
 
         $url = DB::table('urls')->whereRaw('lower(path) like (?)', ["%{$path}%"])->count();
@@ -67,8 +157,14 @@ class UrlService
         }
     }
 
-    protected function validateUrl($url)
+    protected function validateUrl($url): void
     {
+        Validator::make([
+            'long_url' => $url
+        ], [
+            'long_url' => ['required', 'url', 'active_url'],
+        ])->validate();
+        
         if (in_array(parse_url($url, PHP_URL_HOST), config('tinre.restricted_domains'))) {
             throw ValidationException::withMessages([
                 'long_url' => [__('Restricted domain.')],
