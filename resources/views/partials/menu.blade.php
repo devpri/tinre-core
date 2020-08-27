116fb716a4d6a657e38541ac53df00e7e4867e8d
Paul Prisacaru <paul@devpri.com>
2020-08-27 21:46:58 +0300

    update menu

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -4,7 +4,7 @@
             <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z" fill="#fff"/></svg>
         </button>
       <template class="hidden" slot="popover">
-        <ul class="text-gray-700 pt-1 mx-4">
+        <ul class="text-gray-700 shadow pt-1 mx-4">
             <li>
                 <router-link :to="{ name: 'home' }" @click.native="$refs.menu.hide()" class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap w-40">
                     {{ __('Home') }}
